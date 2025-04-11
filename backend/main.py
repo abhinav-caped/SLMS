1a5a0fd9f0c3e40ca6bf968fcad41dbfe7c89600
Abhinav Kumar <abhinavkashyap24112004@gmail.com>
2025-04-12 01:09:57 +0530

    added fronted

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,23 @@ from models import Users, Department,Faculty,Students,Course,Teaches,Classroom,A
 def home():
     return jsonify({"message": "Welcome to the Student Management System API!"})
 
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = Users.query.all()
+    return jsonify([user.to_json() for user in users])
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    new_user = Users(
+        username=data["username"],
+        password=data["password"],
+        role=data["role"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created successfully!"}), 201
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
